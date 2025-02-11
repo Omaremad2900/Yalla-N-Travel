ba0469c2a40c24bb359627f0bf1fe11396bd3827
Omaremad2900 <omaremad2900@gmail.com>
2025-02-11 03:25:08 +0200

    deployed

--- client/src/pages/sellerEditProduct.jsx
+++ client/src/pages/sellerEditProduct.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import FileUpload from "../components/uploadFile"; // Import the FileUpload component
 
 const SellerEditProduct = ({ SellerService }) => {
   const { productId } = useParams();
