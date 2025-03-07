a029d06e4acec68446a1811a7a26327f8116eade
Omaremad2900 <omaremad2900@gmail.com>
2025-03-07 03:12:07 +0200

    done testing

--- client/contants.js
+++ client/contants.js
@@ -1,3 +1,3 @@
 // Define backend service URLs
 const API_URL = 'http://mern-api-service:3000';
-const SOCKET_URL = 'http://mern-api-service:4000';
+const SOCKET_URL = 'http://localhost:8080/socket';
